ea3d5edbc3ac0b517cf87cefff4ac11bf7b67051
McFiggy <96858844+McFiggy@users.noreply.github.com>
2025-11-10 22:12:55 -0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -6,7 +6,11 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"]
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
+// Reuse the client in dev to avoid creating many connections
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
