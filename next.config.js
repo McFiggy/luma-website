687b9cfc03c5be3aea28dbb8ae4df6e33223c6f8
McFiggy <96858844+McFiggy@users.noreply.github.com>
2025-11-10 20:11:58 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { unoptimized: true },
+  // Safety valve: don’t let ESLint block builds. Remove later if you want CI to fail on lint.
+  eslint: { ignoreDuringBuilds: true }
 };
 
 module.exports = nextConfig;
